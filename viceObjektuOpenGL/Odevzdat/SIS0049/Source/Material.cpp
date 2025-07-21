bd4ea7ad8ce73f1a428702bf12b43fc540c754b0
Gumle <masterik676@gmail.com>
2025-07-21 16:30:22 +0200

    Added videos

--- viceObjektuOpenGL/Odevzdat/SIS0049/Source/Material.cpp
+++
@@ -1,8 +0,0 @@
-// Author: SIS0049 - Oliver Siskan
-#include "Material.h"
-
-Material::Material(float ambient, float diffuse, float specular, float shininess, glm::vec3 color)
-
-	: ambient(ambient), diffuse(diffuse), specular(specular), shininess(shininess), color(color)
-{
-}
